81436cf863a367013cebda3dcab9a98a05e03fff
Corwin Kuiper <corwin@kuiper.dev>
2021-06-05 18:11:24 +0100

    use agb image converter 0.2.0

--- build.rs
+++ build.rs
@@ -2,10 +2,12 @@ use agb_image_converter::{convert_image, Colour, ImageConverterConfig, TileSize}
 
 fn main() {
     let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR environment variable must be specified");
-    convert_image(&ImageConverterConfig {
-        transparent_colour: Some(Colour::from_rgb(26, 8, 14)),
-        tile_size: TileSize::Tile8,
-        input_image: "gfx/sprite_sheet.png".into(),
-        output_file: format!("{}/sprite_sheet.rs", out_dir).into(),
-    });
+    convert_image(
+        ImageConverterConfig::builder()
+            .tile_size(TileSize::Tile8)
+            .transparent_colour(Colour::from_rgb(26, 8, 14))
+            .input_image("gfx/sprite_sheet.png".into())
+            .output_file(format!("{}/sprite_sheet.rs", out_dir).into())
+            .build(),
+    );
 }
